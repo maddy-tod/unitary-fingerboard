Normalize desired unitary (probabilities) and fix mean squared error calculation

viz/unitary_grid.py
@@ -22,7 +22,7 @@ from qiskit import BasicAer, execute
 
 from utils.colors import *
 from utils.fonts import *
-from utils.states import PITCH_STATE_NAMES, NUM_QUBITS
+from utils.states import PITCH_STATE_NAMES, NUM_QUBITS, NUM_STATE_DIMS
 
 
 class UnitaryGrid(pygame.sprite.Sprite):
@@ -33,7 +33,7 @@ class UnitaryGrid(pygame.sprite.Sprite):
         self.rect = None
         self.basis_states = PITCH_STATE_NAMES
         self.unitary = None
-        self.desired_matrix = np.zeros((2**NUM_QUBITS, 2**NUM_QUBITS))
+        self.desired_matrix = np.zeros((2 ** NUM_QUBITS, 2 ** NUM_QUBITS))
         self.set_circuit(circuit)
 
     # def update(self):
@@ -92,8 +92,21 @@ class UnitaryGrid(pygame.sprite.Sprite):
         self.draw_unitary_grid(init_bit_str, meas_bit_str)
 
     def cost_desired_vs_unitary(self):
-        mse = np.square(np.square(np.abs(self.desired_matrix)) -
+        mse = np.square(self.desired_matrix -
                         np.square(np.abs(self.unitary))).mean()
         return mse
+
+    def zero_desired_unitary(self):
+        self.desired_matrix = np.zeros((2 ** NUM_QUBITS, 2 ** NUM_QUBITS))
+
+    """Make sum of matrix equal number of quantum state dimensions"""
+    def normalize_desired_unitary(self):
+        matrix_sum = self.desired_matrix.sum()
+        if matrix_sum != 0:
+            scale_factor = NUM_STATE_DIMS / matrix_sum
+            self.desired_matrix *= scale_factor
+            print("desired_matrix.sum(): ", self.desired_matrix.sum())
+        else:
+            print("desired_matrix.sum() was 0")
 
 